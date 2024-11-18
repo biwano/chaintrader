dc913035d7c9181605790ebb02c96a724b44ddf7
biwano <biwano@gmail.com>
2024-11-18 16:09:35 +0100

    base

---
+++ public/index.js
@@ -0,0 +1,12 @@
+import { OpenAPIHono } from "@hono/zod-openapi";
+import { handle } from "hono/vercel";
+const app = new OpenAPIHono().basePath("/api");
+app.get("/", (c) => {
+    return c.json({ message: "Congrats! You've deployed Hono to Vercel0" });
+});
+const handler = handle(app);
+export const GET = handler;
+export const POST = handler;
+export const PATCH = handler;
+export const PUT = handler;
+export const OPTIONS = handler;
